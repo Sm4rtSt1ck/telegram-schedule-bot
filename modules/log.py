45e98fec8e6f85540f021e12f4fda1d2bf1917f5
Sm4rtSt1ck <tranzistor404@gmail.com>
2024-05-29 23:55:24 +0300

    Improv logging

--- modules/log.py
+++ modules/log.py
@@ -4,15 +4,15 @@ from functools import wraps
 from telebot import types
 
 # Set up logging configuration
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%m.%d %H:%M:%S')
 
 def log_user_activity(func):
     @wraps(func)
-    def wrapper(message: types.Message | types.CallbackQuery, *args, **kwargs):
-        user_tag = message.from_user.username if message.from_user.username else message.from_user.id
+    def wrapper(data: types.Message | types.CallbackQuery, *args, **kwargs):
+        user_tag = data.from_user.username if data.from_user.username else data.from_user.id
         try:
-            logging.info(f"User: {user_tag}, Function: {func.__name__}, Message: {message.text}")
+            logging.info(f"User: {user_tag}, Function: {func.__name__}, Message: {data.text}")
         except AttributeError:
-            logging.info(f"User: {user_tag}, Function: {func.__name__}, Callback: {message.data}")
-        return func(message, *args, **kwargs)
+            logging.info(f"User: {user_tag}, Function: {func.__name__}, Callback: {data.data}")
+        return func(data, *args, **kwargs)
     return wrapper
