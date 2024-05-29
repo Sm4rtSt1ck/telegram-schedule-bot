1d691418d8f2980a81341155ebf8291203297709
Sm4rtSt1ck <tranzistor404@gmail.com>
2024-05-29 17:42:42 +0300

    Add custom schedule addition

--- main.py
+++ main.py
@@ -7,27 +7,54 @@ from telebot import TeleBot, types
 
 from modules.schedule import Schedule
 
+
 load_dotenv()
 TOKEN = getenv("TOKEN")
 
-
 bot = TeleBot(TOKEN)
-schedule = Schedule("schedule.csv")
+schedule = Schedule()
 
 user_group: dict[int, str] = dict()
+aboba = True
 
 
 @bot.message_handler(commands=["start"])
 def start(message: types.Message):
     bot.send_message(message.chat.id, getenv("GREETING"))
     bot.send_message(message.chat.id, getenv("SEND_GROUP"))
+    bot.register_next_step_handler(message, set_group)
 
 
-@bot.message_handler(regexp="\\d+\\-\\d+")
 def set_group(message: types.Message):
     user_group[message.from_user.id] = message.text
-    sent_message = bot.send_message(message.chat.id, getenv("SELECT_SCHEDULE"))
-    select_schedule(sent_message)
+    if schedule.check_group(message.text):
+        sent_message = bot.send_message(message.chat.id, getenv("SELECT_SCHEDULE"))
+        select_schedule(sent_message)
+    else:
+        sent_message = bot.send_message(message.chat.id, getenv("GROUP_NOT_FOUND"))
+        add_schedule(sent_message)
+
+
+def add_schedule(message: types.Message) -> None:
+    keyboard = types.InlineKeyboardMarkup([[
+        types.InlineKeyboardButton(getenv("YES"), callback_data="add_schedule"),
+        types.InlineKeyboardButton(getenv("CHANGE_GROUP"), callback_data="change_group")
+    ]])
+    bot.edit_message_text(getenv("GROUP_NOT_FOUND"),
+                          message.chat.id, message.id,
+                          reply_markup=keyboard)
+
+
+def process_schedule(message: types.Message) -> None:
+    if not aboba: return
+    added = schedule.set_schedule(message, user_group[message.from_user.id])
+    if added:
+        sent_message = bot.send_message(message.chat.id, getenv("SCHEDULE_ADDED"))
+        select_schedule(sent_message)
+    else:
+        bot.send_message(message.chat.id, getenv("SCHEDULE_NOT_ADDED"))
+        sent_message = bot.send_message(message.chat.id, getenv("ENTER_SCHEDULE"))
+        button(types.CallbackQuery(sent_message.id, sent_message.from_user, "add_schedule", sent_message.chat, sent_message.json, sent_message))
 
 
 def select_schedule(message: types.Message) -> None:
@@ -45,6 +72,7 @@ def select_schedule(message: types.Message) -> None:
 
 @bot.callback_query_handler(func=lambda callback: True)
 def button(callback: types.CallbackQuery):
+    global aboba
 
     if callback.data == "day":
         show_schedule = schedule.get_day(user_group[callback.from_user.id])
@@ -55,14 +83,33 @@ def button(callback: types.CallbackQuery):
     elif callback.data == "back":
         select_schedule(callback.message)
         return
+    
     elif callback.data == "change_group":
         bot.edit_message_text(getenv("SEND_GROUP"),
-                              callback.message.chat.id, callback.message.id)
+                              callback.message.chat.id,
+                              callback.message.id)
+        
+        aboba = False
+        bot.register_next_step_handler(callback.message, set_group)
         return
 
-    keyboard = types.InlineKeyboardMarkup([[types.InlineKeyboardButton(getenv("BACK"), callback_data="back")]])
-    bot.edit_message_text(show_schedule, callback.message.chat.id,
-                          callback.message.id, reply_markup=keyboard, parse_mode="Markdown")
+    elif callback.data == "add_schedule":
+        keyboard = types.InlineKeyboardMarkup(
+            [[types.InlineKeyboardButton(getenv("BACK"), callback_data="change_group")]])
+
+        bot.edit_message_text(getenv("ENTER_SCHEDULE"),
+                              callback.message.chat.id, callback.message.id,
+                              reply_markup=keyboard, parse_mode="Markdown")
+
+        aboba = True
+        bot.register_next_step_handler(callback.message, process_schedule)
+        return
+
+    keyboard = types.InlineKeyboardMarkup(
+        [[types.InlineKeyboardButton(getenv("BACK"), callback_data="back")]])
+    bot.edit_message_text(show_schedule,
+                          callback.message.chat.id, callback.message.id,
+                          reply_markup=keyboard, parse_mode="Markdown")
 
 
 def main() -> None:
--- modules/schedule.py
+++ modules/schedule.py
@@ -1,18 +1,59 @@
 from datetime import datetime
 import csv
 from os import getenv
+import re
 
 from dotenv import load_dotenv
 from telebot import TeleBot, types
 
 
-load_dotenv("lang2.env")
+load_dotenv("lang.env")
 
 WEEKDAYS = list(map(lambda day: day.replace("_", " "), getenv("WEEKDAYS").split()))
 NOT_FOUND = getenv("NO_SCHEDULE")
 
 
 class Schedule:
+    def check_group(self, group: str) -> bool:
+        with open("schedule.csv", newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Group'] == group: return True
+        return False
+
+    def set_schedule(self, message: types.Message, group: str) -> bool:
+        schedule_entry = message.text.split("\n")
+
+        # Check if the schedule entry matches the required format
+        entry_pattern = re.compile(r'^\d, \d{2}:\d{2}-\d{2}:\d{2}, [0-9a-zA-Zа-яА-Я]+, [0-9a-zA-Zа-яА-Я]+')
+        for line in schedule_entry:
+            if not entry_pattern.match(line):
+                return False
+            components = line.split(', ')
+            day = int(components[0])
+            time = components[1]
+            subject = components[2]
+            room = components[3]
+
+            if not 1 <= day <= 7 or not subject or not room:
+                return False
+
+        with open("schedule.csv", 'a', newline='', encoding='utf-8') as csvfile:
+            csvfile.write("\n")
+            for line in schedule_entry:
+                # Split the schedule entry into components
+                components = line.split(', ')
+                day = int(components[0])
+                time = components[1]
+                subject = components[2]
+                room = components[3]
+
+                # If all checks pass, append the schedule to the CSV file
+                writer = csv.writer(csvfile)
+                writer.writerow([group, day, time, subject, room])
+        
+        return True
+
     def get_day(self, group: str) -> str:
         schedule = []
         with open("schedule.csv", newline='', encoding='utf-8') as csvfile:
@@ -40,4 +81,4 @@ class Schedule:
 
 
     def get_session(self, group: str) -> str:
-        return NOT_FOUND
+        return getenv("IN_DEVELOPING")
